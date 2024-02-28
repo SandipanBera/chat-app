9843bf58d351c8e915727d5d4428c38f9eaa3ea6
Sandipan <sandipanbera7305@gmail.com>
2024-02-28 23:05:23 +0530

    Added JWT authentication to the user model.

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
 const profileSchema = new Schema({
   url: String,
   public_id: String,
@@ -42,8 +43,31 @@ const userSchema = new Schema(
 );
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password=await  bcrypt.hash(this.password, 10);
-  next()
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+// Method to compare and validate the password
+userSchema.methods.comparePassword = async (password) => {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.method.genetrateAccesstoken = async () => {
+  return jwt.sign(
+    {
+      _id: this._id,
+      fullName: this.fullName,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRE,
+    }
+  );
+};
+userSchema.method.genetrateRefreshtoken = async () => {
+  return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_KEY, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+  });
+};
 const User = mongoose.model("User", userSchema); //creating a model of the schema and saving it as User
 export default User;
